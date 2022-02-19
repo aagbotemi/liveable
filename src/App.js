cb382a3495867715dbef54ebad925731396909d6
Abiodun Awoyemi <aabiodunawoyemi@gmail.com>
2022-02-19 18:56:35 +0100

    second chance commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React from"react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import './App.css';
-import Header from "./components/Header"
 import Home from "./Pages/Home"
 
 function App() {
